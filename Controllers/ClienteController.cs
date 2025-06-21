137ddf92c4da163f400e67d4a1d0f65a2ab7dded
Davi Carlos <dacal.davi@outlook.com>
2025-06-20 22:26:27 -0300

    cliente

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using ClienteService.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientesController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    public ClientesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetClientes() => Ok(_context.Clientes.ToList());
+
+    [HttpGet("{id}")]
+    public IActionResult GetCliente(int id)
+    {
+        var cliente = _context.Clientes.Find(id);
+        if (cliente == null) return NotFound();
+        return Ok(cliente);
+    }
+
+    [HttpPost]
+    public IActionResult CreateCliente([FromBody] Cliente cliente)
+    {
+        _context.Clientes.Add(cliente);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetCliente), new { id = cliente.Id }, cliente);
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using ClienteService.Model;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Cliente> Clientes { get; set; }
+}
